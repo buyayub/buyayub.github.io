03e71487e645a1a28360a1b3043fe0e42b9fe28c
buyayub <micourse@protonmail.com>
2022-12-07 12:35:11 -0500

    forgot to build -.-

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,46 +1,4 @@
 var store = [{
-        "title": "To Reign a Computer (WIP)",
-        "excerpt":"Introduction I lack a definitive analysis on how to really become a good programmer so I can continuously improve rather than stagnate. This is not a list of what I know, and it’s not a list of what I will know. It’s more a list of what I want to...","categories": [],
-        "tags": [],
-        "url": "/to-reign/",
-        "teaser": null
-      },{
-        "title": "Project Ideas",
-        "excerpt":"0. Introduction NOTE: (This page has a lot of perspective shift as I’m converting this to first person perspective for a more passive voice; just a disclaimer) A problem I frequently see with programming project ideas are the lack of useful ideas. Things you would actually want to make that...","categories": [],
-        "tags": [],
-        "url": "/project-ideas/",
-        "teaser": null
-      },{
-        "title": "Searle",
-        "excerpt":"My Analysis of Searle’s Chinese Room Searle’s Chinese room is an uncomfortable thought experiment to computationalists, and one I think gets unfairly brushed aside by them. The arguments often end away with saying “the system” is the consciousness, or that it’s flawed in some way. Searle does a well enough...","categories": [],
-        "tags": [],
-        "url": "/searle/",
-        "teaser": null
-      },{
-        "title": "My Progamming Language",
-        "excerpt":"My writing on “reigning” a computer comes with a great question: where on earth do I start? The issue comes by the chasm between application and theory, and the many bridges crossing it. After a bit of soul searching, I thought the funnest way to start is by writing my...","categories": [],
-        "tags": [],
-        "url": "/programming_language/",
-        "teaser": null
-      },{
-        "title": "Plot Analysis",
-        "excerpt":"Intro Plot is the sequence of events that occurs in a story. There is a significant difference between story and plot, in that story represents plot in its own way. Plot always follows a series of cause and events. A -&gt; B that usually goes across a serial linear time....","categories": [],
-        "tags": [],
-        "url": "/plot_analysis/",
-        "teaser": null
-      },{
-        "title": "Quine, On What There Is",
-        "excerpt":"Summarization Definitions Bound Variable A variable that is bound to a specific value or explicit range of values. Ontology A collection of what one takes to exist, both in real and hypothetical contexts. Quine considers the ontological problem to be the question: What is there? The naive answer is to...","categories": [],
-        "tags": [],
-        "url": "/quine_on_what/",
-        "teaser": null
-      },{
-        "title": "Mastery and Knowledge Mapping",
-        "excerpt":"I follow a principle of humility with regards to what I know, denoted by a personal normative philosophy I hope in sharing fully one day. It takes an intuitionist perspective on My knowledge map is a recent idea, brought on by the frustration of travelling through a field blind. The...","categories": [],
-        "tags": [],
-        "url": "/my_knowledge_map/",
-        "teaser": null
-      },{
         "title": "A Note on the Entscheidungsproblem by Alonzo Church",
         "excerpt":"Preliminaries MathML doesn’t work on chromium-based browsers, so you won’t see the proper markup unless you have an extension installed, switch to firefox/safari, or wait until 109 is released (should be in Dec 2022). If you notice any typos or anything like that, send me an email. I’d appreciate it....","categories": [],
         "tags": [],
